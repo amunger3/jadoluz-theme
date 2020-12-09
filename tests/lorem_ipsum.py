a0fdc85e0eeb819f48fe9c934765452c20dc5533
Alex Munger <munger.alex@gmail.com>
2020-12-09 13:37:59 -0500

    u

---
+++ tests/lorem_ipsum.py
@@ -0,0 +1,60 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import collections
+
+print(dcc.__version__) # 0.6.0 or above is required
+print(type(str(1.0)))
+
+class Foo(bar):
+    """
+    docstring
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.dc = {
+            'a': 1,
+            'b': 2
+        }
+        self.gj = collections.namedtuple('s', 'd')
+
+    def funcname(self, parameter_list):
+        """
+        docstring
+        """
+        try:
+            pass
+        except:
+            pass
+        raise NotImplementedError
+    pass
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
+
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-2"', href='/page-2'),
+
+    # content will be rendered in this element
+    html.Div(id='page-content')
+])
+
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    return html.Div([
+        html.H3('You are on page {}'.format(pathname))
+    ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
